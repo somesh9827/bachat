cf0de652d390e5ac09776c5fcf552a93e3221015
somesh9827 <somesh9827@gmail.com>
2014-10-04 11:49:44 +0530

    db added foreign Key

--- db/bachatdb_2014-10-04.sql
+++ db/bachatdb_2014-10-04.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.6.19)
 # Database: bachatdb
-# Generation Time: 2014-10-04 06:06:52 +0000
+# Generation Time: 2014-10-04 06:19:10 +0000
 # ************************************************************
 
 
@@ -48,7 +48,7 @@ CREATE TABLE `customer` (
   `last_name` varchar(25) NOT NULL DEFAULT '',
   `dob` date DEFAULT NULL,
   `gender` char(1) NOT NULL DEFAULT '',
-  `contact` decimal(15,0) DEFAULT NULL,
+  `contact` int(15) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -63,10 +63,12 @@ CREATE TABLE `offer` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `end_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `shopkeeper_id` int(11) NOT NULL,
+  `shop_owner_id` int(11) unsigned NOT NULL,
   `product_name` varchar(50) NOT NULL DEFAULT '',
   `description` varchar(100) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `shop_owner_id` (`shop_owner_id`),
+  CONSTRAINT `offer_ibfk_1` FOREIGN KEY (`shop_owner_id`) REFERENCES `shop_owner` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -80,13 +82,19 @@ CREATE TABLE `shop` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL DEFAULT '',
   `geo_location` varchar(50) DEFAULT NULL,
-  `address_id` int(11) NOT NULL,
-  `owner_id` int(11) NOT NULL,
-  `shop_category_id` int(11) NOT NULL,
+  `address_id` int(11) unsigned NOT NULL,
+  `owner_id` int(11) unsigned NOT NULL,
+  `shop_category_id` int(11) unsigned NOT NULL,
   `opening_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `closing_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `day_closed` varchar(100) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `address_id` (`address_id`),
+  KEY `owner_id` (`owner_id`),
+  KEY `shop_category_id` (`shop_category_id`),
+  CONSTRAINT `shop_ibfk_3` FOREIGN KEY (`shop_category_id`) REFERENCES `shop_category` (`id`),
+  CONSTRAINT `shop_ibfk_1` FOREIGN KEY (`address_id`) REFERENCES `address` (`id`),
+  CONSTRAINT `shop_ibfk_2` FOREIGN KEY (`owner_id`) REFERENCES `shop_owner` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -119,16 +127,18 @@ CREATE TABLE `shop_owner` (
 
 
 
-# Dump of table shopkeeper_login
+# Dump of table shop_owner_login
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `shopkeeper_login`;
+DROP TABLE IF EXISTS `shop_owner_login`;
 
-CREATE TABLE `shopkeeper_login` (
+CREATE TABLE `shop_owner_login` (
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL DEFAULT '',
-  `shopkeeper_id` int(11) NOT NULL,
-  PRIMARY KEY (`email`)
+  `shop_owner_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`email`),
+  KEY `s` (`shop_owner_id`),
+  CONSTRAINT `s` FOREIGN KEY (`shop_owner_id`) REFERENCES `shop_owner` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
